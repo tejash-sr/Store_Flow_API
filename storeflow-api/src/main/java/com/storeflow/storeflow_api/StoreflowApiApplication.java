abd55bb15b6594acc9278e4044a05b4e149a8ce7
Tejash <tejash@storeflow.dev>
2026-04-05 01:37:13 +0530

    test(phase-8): Fix corrupted EmailServiceTest - remove duplicate code (COMMIT #3)

--- storeflow-api/src/main/java/com/storeflow/storeflow_api/StoreflowApiApplication.java
+++ storeflow-api/src/main/java/com/storeflow/storeflow_api/StoreflowApiApplication.java
@@ -2,8 +2,10 @@ package com.storeflow.storeflow_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class StoreflowApiApplication {
 
 	public static void main(String[] args) {
