b7d46aceedba2ca89cdd243188c45a3be6e9f955
Tejash <tejash@storeflow.dev>
2026-04-05 17:41:23 +0530

    Harden CI test reporting and smoke test

--- src/test/java/com/storeflow/storeflow_api/StoreflowApiApplicationTests.java
+++ src/test/java/com/storeflow/storeflow_api/StoreflowApiApplicationTests.java
@@ -4,11 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import com.storeflow.storeflow_api.config.TestMailConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(TestMailConfig.class)
+@TestPropertySource(properties = {
+	"spring.datasource.url=jdbc:h2:mem:testdb",
+	"spring.datasource.driverClassName=org.h2.Driver",
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+	"spring.h2.console.enabled=false"
+})
 class StoreflowApiApplicationTests {
 
 	@Test
