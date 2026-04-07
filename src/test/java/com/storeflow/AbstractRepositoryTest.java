89d79e8fdf9c62def5fd5853e0d141d013f373a5
Tejash <tejash@storeflow.dev>
2026-04-07 16:09:21 +0530

    refactor: correct test architecture with proper PostgreSQL configuration

--- src/test/java/com/storeflow/AbstractRepositoryTest.java
+++ src/test/java/com/storeflow/AbstractRepositoryTest.java
@@ -23,5 +23,6 @@ public abstract class AbstractRepositoryTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
     }
 }
